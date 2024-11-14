9fbcbb2b3942a341e08c3bc8b8659655d9c06723
Jetmir007 <jeku07001@utb.vaxjo.se>
2024-11-14 12:00:53 +0100

    Class

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
 ﻿using Class;
 
 static void Main(){
-    Fordon f = new Fordon("VW", 2019, "Golf", 750);
-
+    int val = 1;
+    Fordon f = new Fordon();
+    while (val != 0){
+        Console.WriteLine("Välj ett alternativ:");
+        Console.WriteLine("1. Skapa ny bil");
+        Console.WriteLine("2. Visa bilar");
+        Console.WriteLine("3. Avsluta");
+        val = Convert.ToInt32(Console.ReadLine());
+        if(val == 1){
+            f.NyBil();
+        }
+        else if(val == 2){
+            f.SkrivUt();
+        }
+        else if(val == 3){
+            break;
+        }
 }
-Main();
+}
+  
--- obj/Debug/net8.0/Class.AssemblyInfo.cs
+++ obj/Debug/net8.0/Class.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Class")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc05c5f545cc7112edb6ca2e9b2275ac5a592cfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993f63ea4cc9c4023e3c009170b8c5ff038586f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Class")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Class")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
